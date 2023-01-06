a5f3764c209ee6ea64e103d29a2227b86d9f93c7
Gustav Hansen <gustav5hansen5@gmail.com>
2023-01-06 14:38:10 +0100

    Implement s1 with Nicki example

--- s1_development_environment/exercise_files/final_exercise/data.py
+++ s1_development_environment/exercise_files/final_exercise/data.py
@@ -1,45 +1,47 @@
-import numpy as np
 import torch
-from torch.utils.data import DataLoader
+import wget
+from torch.utils.data import Dataset
+import numpy as np
+import os
 
-
-def mnist():
-    train = dict()
-    temp0 = np.load(f"/Users/Gustav/Desktop/DesktopV00183/DTU/1st/MLOps.nosync/dtu_mlops/data/corruptmnist/train_0.npz")['images']
-    temp1 = np.load(f"/Users/Gustav/Desktop/DesktopV00183/DTU/1st/MLOps.nosync/dtu_mlops/data/corruptmnist/train_1.npz")['images']
-    temp2 = np.load(f"/Users/Gustav/Desktop/DesktopV00183/DTU/1st/MLOps.nosync/dtu_mlops/data/corruptmnist/train_2.npz")['images']
-    temp3 = np.load(f"/Users/Gustav/Desktop/DesktopV00183/DTU/1st/MLOps.nosync/dtu_mlops/data/corruptmnist/train_3.npz")['images']
-    temp4 = np.load(f"/Users/Gustav/Desktop/DesktopV00183/DTU/1st/MLOps.nosync/dtu_mlops/data/corruptmnist/train_4.npz")['images']
-    temp_data = np.concatenate((temp0, temp1, temp2, temp3, temp4))
-    temp_0 = np.load(f"/Users/Gustav/Desktop/DesktopV00183/DTU/1st/MLOps.nosync/dtu_mlops/data/corruptmnist/train_0.npz")['labels']
-    temp_1 = np.load(f"/Users/Gustav/Desktop/DesktopV00183/DTU/1st/MLOps.nosync/dtu_mlops/data/corruptmnist/train_1.npz")['labels']
-    temp_2 = np.load(f"/Users/Gustav/Desktop/DesktopV00183/DTU/1st/MLOps.nosync/dtu_mlops/data/corruptmnist/train_2.npz")['labels']
-    temp_3 = np.load(f"/Users/Gustav/Desktop/DesktopV00183/DTU/1st/MLOps.nosync/dtu_mlops/data/corruptmnist/train_3.npz")['labels']
-    temp_4 = np.load(f"/Users/Gustav/Desktop/DesktopV00183/DTU/1st/MLOps.nosync/dtu_mlops/data/corruptmnist/train_4.npz")['labels']
-    temp_labels = np.concatenate((temp_0, temp_1, temp_2, temp_3, temp_4))
-    train['images'] = temp_data
-    train['labels'] = temp_labels
-
-    test = np.load("/Users/Gustav/Desktop/DesktopV00183/DTU/1st/MLOps.nosync/dtu_mlops/data/corruptmnist/test.npz")
-
-    """
-    loaders = {
-    'train' : torch.utils.data.DataLoader(train, 
-                                          batch_size=100, 
-                                          shuffle=True, 
-                                          num_workers=1),
+class CorruptMnist(Dataset):
+    def __init__(self, train):
+        self.download_data(train)
+        if train:
+            content = [ ]
+            for i in range(5):
+                content.append(np.load(f"train_{i}.npz", allow_pickle=True))
+            data = torch.tensor(np.concatenate([c['images'] for c in content])).reshape(-1, 1, 28, 28)
+            targets = torch.tensor(np.concatenate([c['labels'] for c in content]))
+        else:
+            content = np.load("test.npz", allow_pickle=True)
+            data = torch.tensor(content['images']).reshape(-1, 1, 28, 28)
+            targets = torch.tensor(content['labels'])
+            
+        self.data = data
+        self.targets = targets
     
-    'test'  : torch.utils.data.DataLoader(test, 
-                                          batch_size=100, 
-                                          shuffle=True, 
-                                          num_workers=1),
-    }
-    print(loaders)
-    for i, (images, labels) in enumerate(loaders['train']):
-        print(images, labels)
-        break
-    """
-    return train, test
+    def download_data(self, train):
+        files = os.listdir()
+        if train:
+            for file_idx in range(5):
+                if f'train_{file_idx}.npy' not in files:
+                    wget.download(f"https://raw.githubusercontent.com/SkafteNicki/dtu_mlops/main/data/corruptmnist/train_{file_idx}.npz")
+        else:
+            if "test.npy" not in files:    
+                wget.download("https://raw.githubusercontent.com/SkafteNicki/dtu_mlops/main/data/corruptmnist/test.npz")
+    
+    def __len__(self):
+        return self.targets.numel()
+    
+    def __getitem__(self, idx):
+        return self.data[idx].float(), self.targets[idx]
 
-if __name__ == '__main__':
-    mnist()
+
+if __name__ == "__main__":
+    dataset_train = CorruptMnist(train=True)
+    dataset_test = CorruptMnist(train=False)
+    print(dataset_train.data.shape)
+    print(dataset_train.targets.shape)
+    print(dataset_test.data.shape)
+    print(dataset_test.targets.shape)
--- s1_development_environment/exercise_files/final_exercise/main.py
+++ s1_development_environment/exercise_files/final_exercise/main.py
@@ -2,76 +2,99 @@ import argparse
 import sys
 
 import torch
-from torch.autograd import Variable
-from torch import optim
-import torch.nn as nn
-import click
 
-from data import mnist
+from data import CorruptMnist
 from model import MyAwesomeModel
 
+import matplotlib.pyplot as plt
 
-@click.group()
-def cli():
-    pass
-
-
-@click.command()
-@click.option("--lr", default=1e-3, help='learning rate to use for training')
-@click.option("--num_epochs", default=100, help='epochs to use for training')
-def train(num_epochs, lr):
-    print("Training day and night")
-    print(lr)
-    print(num_epochs)
-
-    # TODO: Implement training loop here
-    model = MyAwesomeModel().double()
-    train_set, _ = mnist() # train_set['images'], train_set['labels']
-    loss_func = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr = 0.01)
-    model.train()
+class TrainOREvaluate(object):
+    """ Helper class that will help launch class methods as commands
+        from a single script
+    """
+    def __init__(self):
+        parser = argparse.ArgumentParser(
+            description="Script for either training or evaluating",
+            usage="python main.py <command>"
+        )
+        parser.add_argument("command", help="Subcommand to run")
+        args = parser.parse_args(sys.argv[1:2])
+        if not hasattr(self, args.command):
+            print('Unrecognized command')
+            
+            parser.print_help()
+            exit(1)
         
-    for epoch in range(num_epochs):
-        total_steps = len(train_set['images'])
-        for i in range(total_steps):
-            image = torch.from_numpy(train_set['images'][i].reshape((1, 1, 28, 28)))
-            label = torch.from_numpy(train_set['labels'][i])
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
             
-            # gives batch data, normalize x when iterate train_loader
-            x = Variable(image)   # batch x
-            y = Variable(label)   # batch y
+        # use dispatch pattern to invoke method with same name
+        getattr(self, args.command)()
+    
+    def train(self):
+        print("Training day and night")
+        parser = argparse.ArgumentParser(description='Training arguments')
+        parser.add_argument('--lr', default=1e-3)
+        # add any additional argument that you want
+        args = parser.parse_args(sys.argv[2:])
+        print(args)
+        
+        # TODO: Implement training loop here
+        model = MyAwesomeModel()
+        model = model.to(self.device)
+        train_set = CorruptMnist(train=True)
+        dataloader = torch.utils.data.DataLoader(train_set, batch_size=128)
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+        criterion = torch.nn.CrossEntropyLoss()
+        
+        n_epoch = 5
+        for epoch in range(n_epoch):
+            loss_tracker = []
+            for batch in dataloader:
+                optimizer.zero_grad()
+                x, y = batch
+                preds = model(x.to(self.device))
+                loss = criterion(preds, y.to(self.device))
+                loss.backward()
+                optimizer.step()
+                loss_tracker.append(loss.item())
+            print(f"Epoch {epoch+1}/{n_epoch}. Loss: {loss}")        
+        torch.save(model.state_dict(), 'trained_model.pt')
             
-            output = model(x.double())[0]
-            loss = loss_func(output, y)
+        plt.plot(loss_tracker, '-')
+        plt.xlabel('Training step')
+        plt.ylabel('Training loss')
+        plt.savefig("training_curve.png")
+        
+        return model
             
-            # clear gradients for this training step   
-            optimizer.zero_grad()
+    def evaluate(self):
+        print("Evaluating until hitting the ceiling")
+        parser = argparse.ArgumentParser(description='Training arguments')
+        parser.add_argument('load_model_from', default="")
+        # add any additional argument that you want
+        args = parser.parse_args(sys.argv[2:])
+        print(args)
+        
+        # TODO: Implement evaluation logic here
+        model = MyAwesomeModel()
+        model.load_state_dict(torch.load(args.load_model_from))
+        model = model.to(self.device)
+
+        test_set = CorruptMnist(train=False)
+        dataloader = torch.utils.data.DataLoader(test_set, batch_size=128)
+        
+        correct, total = 0, 0
+        for batch in dataloader:
+            x, y = batch
             
-            # backpropagation, compute gradients
-            loss.backward()
-            # apply gradients
-            optimizer.step()
-
-            if (i+1) % 100 == 0:
-                print(f'Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{total_steps}], Loss: {loss.item()}')
-
-
-@click.command()
-@click.argument("model_checkpoint")
-def evaluate(model_checkpoint):
-    print("Evaluating until hitting the ceiling")
-    print(model_checkpoint)
-
-    # TODO: Implement evaluation logic here
-    model = torch.load(model_checkpoint)
-    _, test_set = mnist()
+            preds = model(x.to(self.device))
+            preds = preds.argmax(dim=-1)
+            
+            correct += (preds == y.to(self.device)).sum().item()
+            total += y.numel()
+            
+        print(f"Test set accuracy {correct/total}")
 
 
-cli.add_command(train)
-cli.add_command(evaluate)
-
-
-if __name__ == "__main__":
-    cli()
-
-  
+if __name__ == '__main__':
+    TrainOREvaluate()
