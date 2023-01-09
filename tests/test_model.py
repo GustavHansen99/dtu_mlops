168ec66122474e3fa0e40d55ac49a12f147590a6
Gustav Hansen <gustav5hansen5@gmail.com>
2023-01-09 13:49:45 +0100

    Tests written in functions

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,8 +1,9 @@
 from src.models.model import MyAwesomeModel
 import torch
 
-batch_size = 128
-model = MyAwesomeModel()
-sample = torch.rand(batch_size, 1, 28, 28)
-output = model.forward(sample)
-assert output.shape == torch.Size([batch_size, 10])
+def test_forward_pass():
+    batch_size = 128
+    model = MyAwesomeModel()
+    sample = torch.rand(batch_size, 1, 28, 28)
+    output = model.forward(sample)
+    assert output.shape == torch.Size([batch_size, 10])
